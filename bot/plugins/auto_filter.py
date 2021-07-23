c31ac261cea0a57e522a7e4f07cc49caba2a5eae
Aj7718 <86429612+Aj7718@users.noreply.github.com>
2021-07-23 23:02:09 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -126,7 +126,7 @@ async def auto_filter(bot, update):
                 file_link = f"https://t.me/{bot_.username}?start={unique_id}"
             
             results.append([
-            InlineKeyboardButton("📂 " + f_size, file_names, url=file_link),
+            InlineKeyboardButton("📂 " + file_names, url=file_link),
         ])
             
         
