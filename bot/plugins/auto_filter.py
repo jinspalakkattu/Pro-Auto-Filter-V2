03bb92255a88f29ce7bee88ec0eed498321413f7
Sreejithmadmax <88493309+Sreejithmadmax@users.noreply.github.com>
2021-09-28 14:21:14 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -130,13 +130,13 @@ async def auto_filter(bot, update):
         Send_message=await bot.send_video(
                 chat_id=update.chat.id,
                 video="https://telegra.ph/file/c2c0ff4b927dcc50e7922.mp4",
-                caption=f"Couldn't Find This Movie.Please Try Again Or Search On Our "
-                        f"<b><a href='https://t.me/UFStudio2'>Channel</a></b>. \n\n"
-                        f"ഈ സിനിമയുടെ ഒറിജിനൽ പേര് ഗൂഗിളിൽ പോയി കണ്ടെത്തി അതുപോലെ ഇവിടെ കൊടുക്കുക 🥺",
+                caption=f"Couldn't Find This Movie.Please Try Again Or Search Google Or Not Get Movie Report Admins👉"
+                        f"<b><a href='https://t.me/Myfreak123'>👉Admin</a></b>. \n\n"
+                        f"ഈ സിനിമയുടെ ഒറിജിനൽ പേര് ഗൂഗിളിൽ പോയി കണ്ടെത്തി അതുപോലെ ഇവിടെ കൊടുക്കുക 🥺കൂടുതൽ അറിയാൻ വീഡിയോ കാണുക",
                 parse_mode="html",
                 reply_to_message_id=update.message_id
             )
-        await asyncio.sleep(15) # in seconds
+        await asyncio.sleep(30) # in seconds
         await Send_message.delete()
         # await bot.delete_messages(update.chat.id,update.message_id)
         return  # return if no files found for that query
