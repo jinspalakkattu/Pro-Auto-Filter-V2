ddb24fe51e27e5043f27b106f4e2d5376f58f771
Sreejithmadmax <88493309+Sreejithmadmax@users.noreply.github.com>
2021-09-28 21:48:49 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -222,9 +222,9 @@ async def auto_filter(bot, update):
         reply_markup = InlineKeyboardMarkup(result[0])
 
         try:
-            await bot.send_text(
+            await bot.send_message(
                 chat_id = update.chat.id,
-                caption=f"Found {(len_results)} Results For Your Query: <code>{query}</code>",
+                text=f"Found {(len_results)} Results For Your Query: <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
