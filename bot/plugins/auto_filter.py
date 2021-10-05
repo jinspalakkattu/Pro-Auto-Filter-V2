3069be2421935fa40eaa47f58625be8d8ff7b877
Sreejithmadmax <88493309+Sreejithmadmax@users.noreply.github.com>
2021-10-05 16:21:25 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -120,11 +120,12 @@ async def auto_filter(bot, update):
                 file_link = f"https://t.me/{bot_.username}?start={unique_id}"
             
             
-            results.append([
-            InlineKeyboardButton(file_names, url=file_link),
-            InlineKeyboardButton(file_size, url=file_link)
-        ])
-        
+            results.append(
+                [
+                    InlineKeyboardButton(button_text, url=file_link)
+                ]
+            )
+        #https://telegra.ph/file/57e33c3853f39c8505247.jpg
     else:
         Send_message=await bot.send_video(
                 chat_id=update.chat.id,
