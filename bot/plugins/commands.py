cc48fdd313b802e0e99738e6ef5988ffe6b26d69
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-11-11 01:19:34 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -131,11 +131,14 @@ async def start(bot, update):
     
     reply_markup = InlineKeyboardMarkup(buttons)
     
-    await bot.start_text(
-                chat_id = update.chat.id,
-                start = "CAACAgIAAxkBAAEBIkthdn0aVyTONxI9gAvIOyG-1cD6NAAC1AwAAnqLoEieLyIklDO8mx4E",
-    reply_markup=reply_markup,        reply_to_message_id=update.message_id
-            )
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.START_TEXT.format(
+                update.from_user.first_name),
+        reply_markup=reply_markup,
+        parse_mode="html",
+        reply_to_message_id=update.message_id
+    )
 
 
 
