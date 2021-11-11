019ed87fb9ba9d86e0bdfc1e3712df48a8bf1698
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-11-11 15:45:24 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -30,8 +30,14 @@ async def start(bot, update):
                         InlineKeyboardButton("🔄ᴛʀʏ ᴀɢᴀɪɴ🔄", url="https://t.me/bigmoviesworld")
                     ]
                 ]
-            )
-        )
+                if message.command[1] != "subscribe":
+            btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"checksub#{message.command[1]}")])
+        await client.send_message(
+            chat_id=message.from_user.id,
+            text="**Please Join My Updates Channel to use this Bot!**",
+            reply_markup=InlineKeyboardMarkup(btn),
+            parse_mode="markdown"
+            )      
             return
         except Exception:
             await update.reply_text("Something Wrong. Contact my Support Group")
