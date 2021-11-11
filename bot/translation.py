be348990ed098fd3bdc9dd7aa0e57786465e4811
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-11-11 13:50:58 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,8 +4,23 @@
 
 class Translation(object):
     
-    AUTH_CHANNEL = Noan
+
     
+    
+    auth_channel = environ.get('AUTH_CHANNEL')
+AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
+
+
+
+default_start_msg = """
+**ʜᴀɪ.. ɪ ᴀᴍ ᴍᴏᴠɪᴇꜱ ʟᴏᴋᴀᴍ ᴍᴀʟᴀʏᴀʟᴀᴍ ɢʀᴏᴜᴘ ꜰɪʟᴛᴇʀ ʙᴏᴛ 
+"""
+START_MSG = environ.get('START_MSG', default_start_msg)
+
+FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
+OMDB_API_KEY = environ.get("OMDB_API_KEY", "")
+if FILE_CAPTION.strip() == "":
+    CUSTOM_FILE_CAPTION=None
     START_MSG = """
 **Hi, I'm Media Search bot**
 Here you can search files in inline mode. Just press follwing buttons and start searching.
