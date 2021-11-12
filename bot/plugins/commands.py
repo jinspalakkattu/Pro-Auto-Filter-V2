086ab4de32ab6ff379f13ef7a5677e1bd5e0412a
Jins Mathew <68688737+jinspalakkattu@users.noreply.github.com>
2021-11-12 11:48:44 +0300

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -13,6 +13,7 @@ db = Database()
 @Client.on_message(filters.command(["start"]) & filters.private, group=1)
 async def start(bot, update):
     update_channel = "@bigmoviesworld"
+    file_uid = False
     
     try:
         file_uid = update.command[1]
